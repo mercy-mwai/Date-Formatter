d67b40f41e715d6709ea399e0ceb6677a461b302
mercy-mwai <mercyneemam@gmail.com>
2024-05-22 15:23:50 +0300

    My last commit

--- script.js
+++ script.js
@@ -8,11 +8,15 @@ const hours=date.getHours();
 const minutes=date.getMinutes();
 const formattedDate=`${day}-${month}-${year}`;
 currentDateParagraph.textContent=formattedDate;
-const exampleSentence="selur pmaCedoCeerf".split(" ").reverse();
-console.log("exampleSentence");
 dateOptionsSelectElement.addEventListener("change",()=>{
 switch(dateOptionsSelectElement.value){
     case "yyyy-mm-dd":
-        currentDateParagraph.textContent=formattedDate;
+        currentDateParagraph.textContent=formattedDate.split("-").reverse().join("-");
+        break;
+        case "mm-dd-yyyy-h-mm":
+            currentDateParagraph.textContent=`${month}-${day}-${year}${hours}Hours,${minutes}Minutes`;
+            break;
+            default:
+                currentDateParagraph.textContent("formattedDate");
 }
 })
